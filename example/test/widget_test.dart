a4cc6ae6530b6785a2c085fb0e26bec5d6e21183
Andrea <andrea.bittau@gmail.com>
2026-01-09 11:39:00 +0100

    fix(example): navigate to counter page in widget test

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -15,6 +15,10 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
+    // Navigate to Counter Page
+    await tester.tap(find.text('Counter & Computed'));
+    await tester.pumpAndSettle();
+
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
