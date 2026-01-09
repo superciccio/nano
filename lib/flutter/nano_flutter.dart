61ce04f69451e2869edb9a4e9190aeee73b15ece
Andrea <andrea.bittau@gmail.com>
2026-01-09 12:32:54 +0100

    feat: add Tuple Watch extensions and avoid_nested_watch lint

--- lib/flutter/nano_flutter.dart
+++ lib/flutter/nano_flutter.dart
@@ -258,3 +258,37 @@ extension ValueListenableWatcher<T> on ValueListenable<T> {
     return Watch<T>(this, builder: builder);
   }
 }
+
+/// Ergonomic extensions for Tuple (Record) of 2 [ValueListenable]s.
+extension NanoTuple2Extension<T1, T2> on (ValueListenable<T1>, ValueListenable<T2>) {
+  /// Watches both atoms and rebuilds when either changes.
+  ///
+  /// Example:
+  /// ```dart
+  /// (count, name).watch((context, c, n) {
+  ///   return Text('$n: $c');
+  /// })
+  /// ```
+  Widget watch(Widget Function(BuildContext context, T1 v1, T2 v2) builder) {
+    return WatchMany(
+      [this.$1, this.$2],
+      builder: (context) => builder(context, this.$1.value, this.$2.value),
+    );
+  }
+}
+
+/// Ergonomic extensions for Tuple (Record) of 3 [ValueListenable]s.
+extension NanoTuple3Extension<T1, T2, T3> on (
+  ValueListenable<T1>,
+  ValueListenable<T2>,
+  ValueListenable<T3>
+) {
+  /// Watches all 3 atoms and rebuilds when any changes.
+  Widget watch(Widget Function(BuildContext context, T1 v1, T2 v2, T3 v3) builder) {
+    return WatchMany(
+      [this.$1, this.$2, this.$3],
+      builder: (context) =>
+          builder(context, this.$1.value, this.$2.value, this.$3.value),
+    );
+  }
+}
